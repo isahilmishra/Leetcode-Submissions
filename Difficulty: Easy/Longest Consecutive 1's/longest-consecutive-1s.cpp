e17b23af53b00825b14dc0d6b66edb445a69cf10
Sahil Raj <isahilmishra0175@gmail.com>
2026-08-25 09:02:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Longest Consecutive 1's/longest-consecutive-1s.cpp
+++ Difficulty: Easy/Longest Consecutive 1's/longest-consecutive-1s.cpp
@@ -1,21 +1,26 @@
 class Solution {
   public:
+    vector<int>convertToBinary(int n){
+        vector<int>res;
+        while(n>0){
+            if(n%2==1) res.push_back(1);
+            else res.push_back(0);
+            n/=2;
+        }
+        reverse(res.begin(),res.end());
+        return res;
+    }
     int maxConsecutiveOnes(int n) {
         // code here
-        vector<int>ans;
-        while(n>0){
-            int digit= n%2;
-            ans.push_back(digit);
-            n=n/2;
-        }
-        ans.push_back(n);
-        
-        int  l=0, r=0 , maxLen=0;
-        while(r<ans.size()){
-            if(ans[r]==0){
+        vector<int>arr= convertToBinary(n);
+        int t=arr.size();
+        int l=0; int r=0;
+        int maxLen=0;
+        while(r<t){
+            if(arr[r]==0){
                 l=r+1;
             }
-            maxLen= max(maxLen, r-l+1);
+            maxLen=max(maxLen,r-l+1);
             r++;
         }
         return maxLen;
